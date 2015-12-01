24154d3ab6d8905210f7c7042e2a8ecc27c7dfa8
Cole Kettler <me@colekettler.com>
2015-12-01 11:47:04 -0500

    Create gulp test task.

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,17 @@ var _ = require('lodash');
 var gulp = require('gulp');
 var cache = require('gulp-cached');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
+var tldr = require('mocha-tldr-reporter');
 
-var jsFiles = ['*.js', 'src/*.js'];
+var jsFiles = ['*.js', 'src/*.js', 'test/*.js'];
+var testFiles = ['test/*.js'];
 
-gulp.task('default', ['watch', 'lint', 'start']);
-
-gulp.task('lint', function() {
-  return gulp.src(jsFiles)
-    .pipe(cache('linting'))
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+// Detect state within tasks
+var flags = {
+  isWatching: false,
+};
 
 // Default Nodemon options
 var nodemonOpts = {
@@ -27,6 +26,21 @@ var nodemonOpts = {
     },
 };
 
+gulp.task('default', ['watch', 'lint', 'test', 'start']);
+
+gulp.task('lint', function() {
+  return gulp.src(jsFiles)
+    .pipe(cache('linting'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', function() {
+  var reporter = flags.isWatching ? tldr : 'nyan';
+  return gulp.src(testFiles, { read: false })
+    .pipe(mocha({ reporter: reporter }));
+});
+
 gulp.task('start', function() {
   return nodemon(nodemonOpts);
 });
@@ -37,5 +51,6 @@ gulp.task('debug', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(jsFiles, ['lint']);
+  flags.isWatching = true;
+  gulp.watch(jsFiles, ['lint', 'test']);
 });
